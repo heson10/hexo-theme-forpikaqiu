a7ede3683e03839097067c0f69ab65ffe8994fd5
heson525 <heson525@163.com>
2020-11-13 23:20:06 +0800

    first commit

---
+++ source/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    //导航栏
+    $("#menu-toggle-mobile").click(function(){
+      $(this).toggleClass('active');
+      $('#menu-mobile').toggleClass('active');
+    });
+
+
+
+
+  });
